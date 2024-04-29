a1169fc7cdc5c6c89406ba02bd1075e3cd66c647
Kaustubh Dharme <109369749+Active2003@users.noreply.github.com>
2024-04-30 02:46:54 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 4000);
 
-const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://localhost/medbuddy')
+const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb+srv://dharmekaustubh2003:p5suNfItx6nVYjj8@cluster0.d4d91pj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 module.exports = {
   Port: Port,
