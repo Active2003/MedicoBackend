d8a24a1feec1f36b331f69c1a5341fde760f2459
Kaustubh Dharme <109369749+Active2003@users.noreply.github.com>
2024-04-30 02:15:54 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,10 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 4000);
 
-// Use environment variables for MongoDB URL
-const MongodbUrl = process.env.NODE_ENV === 'production' ?
-  process.env.MONGODB_URL_PRODUCTION :
-   (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://localhost/testmedbuddy');
-  
+const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://localhost/medbuddy')
 
 module.exports = {
   Port: Port,
   MongodbUrl: MongodbUrl
-};
+}
