4d2fda4b52a208ada130dd131f9019e5bf06544f
Kaustubh Dharme <109369749+Active2003@users.noreply.github.com>
2024-04-30 02:01:18 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 4000)
 // Use environment variables for MongoDB URL
 const MongodbUrl = process.env.NODE_ENV === 'production' ?
   process.env.MONGODB_URL_PRODUCTION :
-   (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://localhost:27017/medico');
+   (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://localhost/testmedbuddy');
   
 
 module.exports = {
